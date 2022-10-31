e4f532f949e7109132ccd68fc60f2b9ad2c95049
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-10-31 21:26:38 +0600

    social home section initialized

--- src/pages/Home/SocialHome/SocialHome.js
+++ src/pages/Home/SocialHome/SocialHome.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React from "react";
 
 const SocialHome = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="px-4 py-6">
+        <div class="grid grid-cols-4 gap-4 "> 
+          <div class="col-span-1 ">
+                <div className="">
+                <h2 className="text-2xl font-medium mb-2">Key Features</h2>
+                <div className="bg-gray-50 rounded shadow-sm">
+                    <div className="flex items-center bg-primary text-white pl-4 py-3 rounded">
+                    <i class="fa-solid fa-clapperboard mr-2"></i>
+                        <h4>Social (News Feed)</h4>
+                    </div> 
+                    <hr />
+                    <div className="flex items-center pl-4 py-3 rounded">
+                    <i class="fa-solid fa-video mr-2"></i>
+                        <h4>Meeting</h4>
+                    </div>
+                        <hr />
+                    <div className="flex items-center pl-4 py-3 rounded"><i class="fa-solid fa-comment-dots mr-2"></i>
+                        <h4>Chatting</h4>
+                    </div>
+                        <hr />
+                    <div className="flex items-center pl-4 py-3 rounded"><i class="fa-solid fa-play mr-2"></i>
+                        <h4>Play</h4>
+                    </div>
+                </div>
+                </div>
+
+                <div class="mt-2">
+                <h2 className="text-2xl font-medium mb-2">Account</h2>
+                <div className="rounded bg-gray-50 shadow-sm">
+                    <div className="flex items-center  text-dark pl-4 py-3 rounded">
+                        
+                    <i class="fa-solid fa-user mr-2"></i>
+                        <h4>My Profile</h4>
+                    </div> 
+                    <hr />
+                    <div className="flex items-center text-dark pl-4 py-3 rounded">
+                    <i class="fa-solid fa-user-pen mr-2"></i>
+                        <h4>Edit Profile</h4>
+                    </div>  
+                </div>
+            </div> 
+
+                <div class="mt-2 ">
+                <h2 className="text-2xl font-medium mb-2">Additional Settings</h2>
+                <div className="bg-gray-50 rounded shadow-sm">
+                    <div className="flex items-center  text-dark pl-4 py-3 rounded">
+                    <i class="fa-solid fa-gear mr-2"></i>
+                        <h4>Settings</h4>
+                    </div> 
+                    <hr />
+                    <div className="flex items-center text-dark pl-4 py-3 rounded">
+                    <i class="fa-solid fa-circle-dot mr-2"></i>
+                        <h4>Options</h4>
+                    </div>  
+                </div>
+            </div> 
+            </div> 
+
+          
+          <div class="col-span-3 ">07</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SocialHome;
